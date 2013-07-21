fe8770549ee24bd958629902fa8b9e9d85e177bb
afoat <afoat.58967548@nowhere.fake>
2013-07-20 23:16:45 -0500

    Adding in remainder of the project

---
+++ CommonTestConsole/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace CommonTestConsole
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Common.Collections.Generic;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10000000;
+            int freq = n / 100;
+
+            Console.WriteLine("Building tree of {0}, with updates every {1}", n, freq);
+
+            int[] numbers = GetUniqueRandomNumbers(n);
+
+            IBinarySearchTree<int> bst = new AVLTree<int>();
+
+            Console.WriteLine("Starting to build the tree");
+
+            foreach (int num in numbers)
+            {
+                bst.Insert(num);
+                if (num % freq == 0)
+                    Console.Write(".");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Finished");
+            Console.Write("Checking tree integrity");
+
+            int previous = -1;
+            foreach (int num in bst.InOrderIterator)
+            {
+                if (previous > num)
+                    throw new InvalidOperationException();
+
+                previous = num;
+            }
+
+            Console.WriteLine(" - Passed");
+        }
+
+        private static int[] GetUniqueRandomNumbers(int n)
+        {
+            Random rng = new Random();
+            Dictionary<int, int> numbers = new Dictionary<int, int>(n);
+            while (numbers.Count < n)
+            {
+                int num = rng.Next(0, int.MaxValue);
+                if (!numbers.ContainsKey(num))
+                    numbers.Add(num, num);
+            }
+
+            int[] keys = numbers.Keys.ToArray();
+            numbers = null;
+            return keys;
+        }
+    }
+}
